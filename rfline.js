2e3c15d2ef30b379eca5789e8514c4a544a810f7
Dickson Tam <dicksont650@gmail.com>
2015-04-26 07:17:31 -0700

    Removed external dependency on extend. Bumped version to 0.0.2.

--- rfline.js
+++ rfline.js
@@ -26,7 +26,23 @@
 
 var fs = require('fs');
 var path = require('path');
-var extend = require('extend');
+
+var extend = function ( defaults, options ) {
+    var extended = {};
+    var prop;
+
+    for (prop in defaults) {
+        if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
+            extended[prop] = defaults[prop];
+        }
+    }
+    for (prop in options) {
+        if (Object.prototype.hasOwnProperty.call(options, prop)) {
+            extended[prop] = options[prop];
+        }
+    }
+    return extended;
+};
 
 function RFLine(fpath, opts) {
   var cb = this.cb = {};
